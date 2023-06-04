369502c4b8440125e4b3e3da87ede351e68d9c17
phptuts <glaserpower@gmail.com>
2023-06-03 22:21:36 -0700

    chrome extensions complete

--- popup.js
+++ popup.js
@@ -3,40 +3,41 @@ const h2 = document.querySelector("h2");
 let isTakingOff = false;
 let intervalId;
 let countDown = 5;
+async function onBlastOff() {
+  try {
+    const [activeTab] = await chrome.tabs.query({ active: true });
+    const replyMessage = await chrome.tabs.sendMessage(activeTab.id, {
+      type: "takeoff",
+    });
+  } catch (e) {
+    console.log(e, "error");
+  }
+  return;
+
+  if (isTakingOff) {
+    return;
+  }
+  isTakingOff = true;
+  intervalId = setInterval(startCountDown, 1000);
+}
 
 async function startCountDown() {
-    if (countDown == 0) {
-        h2.textContent = "TAKE OFF!";
-        clearInterval(intervalId);
-        const [activeTab] = await chrome.tabs.query({ active: true });
-        const replyMessage =  await chrome.tabs.sendMessage(activeTab.id, {
-            'type': 'takeoff'
-        });
-        console.log(replyMessage, 'replyMessage')
-        setTimeout(() => {
-            isTakingOff = false;
-            countDown = 5;
-            h2.textContent = countDown;
-        }, 3000);
-        return;
-    }
-    countDown -= 1;
-    h2.textContent = countDown;
-}
-
-async function onBlastOff() {
+  if (countDown == 0) {
+    h2.textContent = "TAKE OFF!";
+    clearInterval(intervalId);
     const [activeTab] = await chrome.tabs.query({ active: true });
-    const replyMessage =  await chrome.tabs.sendMessage(activeTab.id, {
-        'type': 'takeoff'
+    const replyMessage = await chrome.tabs.sendMessage(activeTab.id, {
+      type: "takeoff",
     });
-    return
-
-    if (isTakingOff) {
-        return;
-    }
-    isTakingOff = true;
-    intervalId = setInterval(startCountDown, 1000);
+    setTimeout(() => {
+      isTakingOff = false;
+      countDown = 5;
+      h2.textContent = countDown;
+    }, 3000);
+    return;
+  }
+  countDown -= 1;
+  h2.textContent = countDown;
 }
 
-
-button.addEventListener('click', onBlastOff);
+button.addEventListener("click", onBlastOff);
